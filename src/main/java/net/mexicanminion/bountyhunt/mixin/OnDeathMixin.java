eb1d18a2d23c773720d12c957604283f17ea3456
Javier <moncadajavier2@yahoo.com>
2024-02-04 17:48:47 -0500

    Update: issue #1 Fixed (When I kill someone indirectly, i dont get the reward)

--- src/main/java/net/mexicanminion/bountyhunt/mixin/OnDeathMixin.java
+++ src/main/java/net/mexicanminion/bountyhunt/mixin/OnDeathMixin.java
@@ -4,13 +4,9 @@ import net.mexicanminion.bountyhunt.managers.BountyManager;
 import net.mexicanminion.bountyhunt.managers.CurrencyManager;
 import net.mexicanminion.bountyhunt.managers.RewardManager;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.packet.s2c.play.SubtitleS2CPacket;
-import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,7 +23,6 @@ public class OnDeathMixin {
 
     /**
      * INJECTION TO onDeath IN MINECRAFT CODE
-     *
      * onPlayerDeath()
      * Description: This mixin is used to check if a player has a bounty on them when they die.
      *              If they do, the player who killed them will receive the bounty and the player who died will lose the bounty.
@@ -40,7 +35,6 @@ public class OnDeathMixin {
         //this.sendMessage(Text.of("You have added  diamond blocks to's bounty"), false);
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
         checkBounty(player, damageSource);
-
     }
 
     /**
@@ -52,11 +46,10 @@ public class OnDeathMixin {
      */
     @Unique
     private void checkBounty(ServerPlayerEntity target, DamageSource damageSource){
-        //TODO: KNOWN ISSUE!!!!  If a player kills the bounty target indirectly, they should receive the bounty
         if(!BountyManager.getBounty(target.getUuid())){
             return;
         }
-        if((damageSource.getAttacker() == null)){
+        if(target.getAttacker() == null){
             return;
         }
         if(damageSource.isIndirect()) {
@@ -66,12 +59,12 @@ public class OnDeathMixin {
         if(BountyManager.getBounty(target.getUuid())){
             //if they did, give the bounty to the player who killed them (SET REWARD, REMOVE BOUNTY AND CURRENCY)
             BountyManager.setBounty(target.getUuid(), false);
-            RewardManager.setReward(damageSource.getAttacker().getUuid(), CurrencyManager.getCurrency(target.getUuid()));
+            RewardManager.setReward(target.getAttacker().getUuid(), CurrencyManager.getCurrency(target.getUuid()));
             CurrencyManager.emptyCurrency(target.getUuid());
-            damageSource.getAttacker().sendMessage(Text.of("You have claimed " + target.getEntityName() + "'s bounty!"));
+            target.getAttacker().sendMessage(Text.of("You have claimed " + target.getEntityName() + "'s bounty!"));
             target.sendMessage(Text.of("You have been cleared of your burden"), false);
             for (ServerPlayerEntity players : server.getPlayerManager().getPlayerList()) {
-                if ((players == damageSource.getAttacker()) || (players == target)) {
+                if ((players == target) || (players == target.getAttacker())) {
                     continue;
                 }
                 players.sendMessage(Text.of("The bounty on " + target.getEntityName() + " has been claimed!"), false);
